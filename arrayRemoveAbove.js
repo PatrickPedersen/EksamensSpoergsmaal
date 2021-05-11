bb4a00939ff10f6050767c50a17de9abf2279d47
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2021-05-11 21:19:11 +0200

    Remove Above

--- arrayRemoveAbove.js
+++ arrayRemoveAbove.js
@@ -5,5 +5,21 @@
     Vis hvordan funktionen kan gøres til en metode i en class.
 
     Beskriv koden med et flowchart og offentliggør den på et git repository.
+
+    Lavet af Patrick
 */
 
+function removeAbove(array, num) {
+    let result = []
+    // Looper igennem array
+    for (let index in array) {
+        // Hvis array er over eller lig med num, så skub værdien til result array.
+        if (array[index] <= num) {
+            result.push(array[index])
+        }
+    }
+
+    return result
+}
+
+console.log(removeAbove([1, 2, 3, 4, 5], 2))
